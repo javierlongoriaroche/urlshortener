70a8966432620e0dc319c15f61fe2c09c9f2b810
Jabc0 <javiergonzalezlongoria@gmail.com>
2023-09-01 10:11:15 +0200

    ADD api shorturl endpoints

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 require('dotenv').config();
+
 const express = require('express');
 const cors = require('cors');
 const app = express();
+const dns = require('dns');
 
 // Basic Configuration
 const port = process.env.PORT || 3000;
@@ -17,6 +19,56 @@ app.get('/', function(req, res) {
 // Your first API endpoint
 app.get('/api/hello', function(req, res) {
   res.json({ greeting: 'hello API' });
+});
+
+app.use(express.json());
+
+
+let nextShortUrl = 1;
+const urlDatabase = {};
+
+// Function to validate URLs using dns.lookup
+function validateUrl(url, callback) {
+  const match = url.match(/^(https?:\/\/)?([^/]+)/i);
+
+  if (!match) {
+    callback(false);
+  } else {
+    const hostname = match[2];
+    dns.lookup(hostname, (err) => {
+      callback(!err);
+    });
+  }
+}
+
+app.post('/api/shorturl', (req, res) => {
+  const { url } = req.body;
+
+  // Validate the URL
+  validateUrl(url, (isValid) => {
+    if (!isValid) {
+      return res.json({ error: 'invalid url' });
+    }
+
+    // Store the URL in the database and assign a short_url
+    const shortUrl = nextShortUrl;
+    urlDatabase[shortUrl] = url;
+    nextShortUrl++;
+
+    res.json({ original_url: url, short_url: shortUrl });
+  });
+});
+
+app.get('/api/shorturl/:short_url', (req, res) => {
+  const { short_url } = req.params;
+
+  // Check if short_url exists in the database
+  if (!urlDatabase[short_url]) {
+    return res.json({ error: 'short_url not found' });
+  }
+
+  // Redirect to the original URL
+  res.redirect(urlDatabase[short_url]);
 });
 
 app.listen(port, function() {
