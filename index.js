54afe7502ebd117a4ee5e01be10180b25f6c9697
Jabc0 <javiergonzalezlongoria@gmail.com>
2023-09-01 10:32:44 +0200

    CHANGE to woek with url_encoded params

--- index.js
+++ index.js
@@ -19,29 +19,16 @@ app.get('/api/hello', function(req, res) {
   res.json({ greeting: 'hello API' });
 });
 
-app.use(express.json());
 
+
+app.use(express.urlencoded({ extended: true }));
 
 let nextShortUrl = 1;
 const urlDatabase = {};
 
-function validateUrl(url, callback) {
-  const match = url.match(/^(https?:\/\/)?([^/]+)/i);
-
-  if (!match) {
-    callback(false);
-  } else {
-    const hostname = match[2];
-    dns.lookup(hostname, (err) => {
-      callback(!err);
-    });
-  }
-}
-
-app.use(express.json());
-
 app.post('/api/shorturl', (req, res) => {
-    const { url } = req.body;
+    const url = req.body.url;
+    console.log(url)
 
     const urlPattern = /^(https?:\/\/)(www\.)?[\w.-]+\.[a-z]{2,}(\/\S*)?$/i;
   if (!urlPattern.test(url)) {
@@ -55,15 +42,7 @@ app.post('/api/shorturl', (req, res) => {
     res.json({ original_url: url, short_url: shortUrl });
 });
 
-app.get('/api/shorturl/:short_url', (req, res) => {
-    const { short_url } = req.params;
 
-    if (!urlDatabase[short_url]) {
-    return res.json({ error: 'short_url not found' });
-  }
-
-    res.redirect(urlDatabase[short_url]);
-});
 
 
 
